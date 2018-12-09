a4a48e43986a51fe83d0830bac7c10ba1fcad7f5
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2018-12-09 09:35:08 -0800

    Fixes for task2 and task3

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,71 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+from application import Application
 from contact import Contact
 import unittest
 
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.app = Application()
 
     def test_add_contact(self):
-        driver = self.driver
-        self.login(driver)
-        self.add_contact(driver, Contact(firstname="Alla", lastname="Yeroshchenko", nickname="Alochka", title="title",
+        self.app.login("Admin", "secret")
+        self.app.add_contact(Contact(firstname="Alla", lastname="Yeroshchenko", nickname="Alochka", title="title",
                                          company="company", address="address", home="home", mobile="111111", work="222222",
                                          fax="333333", email="yeroshchenko@gmail.com", bday="17", bmonth="July", byear="2000"))
-        self.logout(driver)
-
-    def logout(self, driver):
-        driver.find_element_by_link_text("Logout").click()
-
-    def add_contact(self, driver, contact):
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys(contact.title)
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys(contact.company)
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys(contact.address)
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.home)
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys(contact.mobile)
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys(contact.work)
-        driver.find_element_by_name("fax").clear()
-        driver.find_element_by_name("fax").send_keys(contact.fax)
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
-        Select(driver.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys(contact.byear)
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def login(self, driver):
-        self.open_home_page(driver)
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-
-
-    def open_home_page(self, driver):
-        driver.get("http://localhost/addressbook/")
+        self.app.logout()
 
 
     def tearDown(self):
-        self.driver.quit()
+        self.app.destroy()
 
 
 if __name__ == "__main__":
--- test_add_group.py
+++ test_add_group.py
@@ -1,65 +1,26 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from application import Application
 from group import Group
 import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.app=Application()
 
     def test_add_group(self):
-        wd = self.wd
-        self.login(wd, "Admin", "secret")
-        self.create_group(wd, Group("Group1", "Header1", "footer1"))
-        self.logout(wd)
+        self.app.login("Admin", "secret")
+        self.app.create_group(Group("Group1", "Header1", "footer1"))
+        self.app.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, "Admin", "secret")
-        self.create_group(wd, Group("", "", ""))
-        self.logout(wd)
+        self.app.login("Admin", "secret")
+        self.app.create_group(Group("", "", ""))
+        self.app.logout()
 
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def to_group_page(self, wd):
-        # return to group page
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.to_group_page(wd)
-
-
-    def login(self, wd, login, password):
-        # login
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # open homepage
-        wd.get("http://localhost/addressbook/group.php")
-
-    
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 if __name__ == "__main__":
     unittest.main()
