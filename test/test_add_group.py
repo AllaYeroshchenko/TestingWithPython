a041239f3a70b3a7aeadf176bb628ce0d2c340d6
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2018-12-09 22:45:00 -0800

    Task 6

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import time
 
 def test_add_group(app):
-    time.sleep(5)
     app.session.login("Admin", "secret")
     app.group.create(Group("Group1", "Header1", "footer1"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
-    time.sleep(5)
     app.session.login("Admin", "secret")
     app.group.create(Group("", "", ""))
     app.session.logout()
