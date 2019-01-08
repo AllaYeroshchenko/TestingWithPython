b372d8da81cd658d4a4d77424400cd2212cc7262
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2019-01-08 14:53:59 -0800

    Task 16

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,7 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters+string.digits+" "*10
+    symbols = string.ascii_letters+string.digits
     return prefix+"".join([random.choice(symbols) for i in range(random.randrange(1, maxlen))])
 
 testdata=[Contact(firstname="", lastname="", address="", nickname="", company="", title="", fax="", home="", mobile="",
