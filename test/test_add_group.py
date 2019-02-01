86c252b463d0dbef7c076e9f1895c534eb836b3b
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2019-01-31 22:44:54 -0800

    Task 20 _3

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ from model.group import Group
 
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, db, json_groups):
+def test_add_group(app, db, json_groups, check_ui):
     group=json_groups
     #old_groups = app.group.get_group_list()
     old_groups = db.get_group_list()
@@ -16,6 +16,11 @@ def test_add_group(app, db, json_groups):
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    if check_ui:
+        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
 
 
 
