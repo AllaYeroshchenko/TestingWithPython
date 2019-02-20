beb51dee12eb18515c0485cd059e51af14f09d37
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2019-02-20 13:00:32 -0800

    allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 #import pytest
+#import allure_pytest
 #from data.groups import testdata
 
 
@@ -8,10 +9,11 @@ from model.group import Group
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, db, json_groups, check_ui):
     group=json_groups
+ #   with allure_pytest.step('Given a group list'):
     old_groups = db.get_group_list()
+ #   with allure_pytest.step('When I add a group %s to the list' % group):
     app.group.create(group)
-    #assert len(old_groups)+1 == app.group.count()
-    #new_groups = app.group.get_group_list()
+  #  with allure_pytest.step('Then the new group list is equal to the old list with the added group'):
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
